011928fc227ec04ac2eb6da388d921326880abd3
Kate Volkova <ekaterinavolkovaalex@gmail.com>
2018-08-10 15:36:16 +0300

    feat: add dropzone to load xlsx files

--- src/app/containers/BatchUpdate/index.tsx
+++ src/app/containers/BatchUpdate/index.tsx
@@ -4,6 +4,8 @@ import { connect } from 'react-redux';
 import { RootState } from 'core/reducers';
 import { fetchAllCourses } from 'core/actions';
 import { getCoursesNames } from 'core/selectors/courses';
+import Dropzone from 'react-dropzone';
+// import { read, utils } from 'xlsx';
 
 // import { classNames } from 'core/styles';
 
@@ -52,6 +54,8 @@ class BatchUpdate extends React.Component<any, State> {
             coursesDropdownOpen: false,
             tasksValue: 'choose task',
             coursesValue: 'choose course',
+            disabled: false,
+            files: [],
         };
     }
 
@@ -59,6 +63,33 @@ class BatchUpdate extends React.Component<any, State> {
         this.props.fetchCourses();
         // this.props.fetchTasks();
     }
+
+    onDrop = (files: any) => {
+        // const reader = new FileReader();
+        // reader.onload = e => {
+        //     // let filename = files[0].name;
+        //     let binary = '';
+        //     const bytes = new Uint8Array(e.target.result);
+        //     const length = bytes.byteLength;
+        //     for (let i = 0; i < length; i++) {
+        //         binary += String.fromCharCode(bytes[i]);
+        //     }
+        //     const Workbook = read(binary, { type: 'binary', cellDates: true });
+        //     const sheetName = Workbook.SheetNames[0];
+        // const json = utils.sheet_to_json(Workbook.Sheets[sheetName], { header: 1 });
+
+        // do whatever you want with the file content
+        // };
+        // reader.onabort = () => {};
+        // reader.onerror = () => {};
+
+        // reader.readAsArrayBuffer(files[0]);
+
+        this.setState({
+            files,
+            disabled: true,
+        });
+    };
 
     toggle(dropdownName: string) {
         this.setState({
@@ -74,48 +105,59 @@ class BatchUpdate extends React.Component<any, State> {
 
     render() {
         return (
-            <div className="row">
-                <div className="col-sm-6">
-                    <Dropdown
-                        isOpen={this.state.coursesDropdownOpen}
-                        toggle={this.toggle.bind(this, 'coursesDropdownOpen')}
-                    >
-                        <DropdownToggle caret={true}>{this.state.coursesValue}</DropdownToggle>
-                        <DropdownMenu>
-                            {this.props.courses.map((course: any) => {
-                                return (
-                                    <DropdownItem
-                                        key={course._id}
-                                        onClick={(event: any) => this.select(event, 'coursesValue')}
-                                    >
-                                        {course.name}
-                                    </DropdownItem>
-                                );
-                            })}
-                        </DropdownMenu>
-                    </Dropdown>
-                </div>
-                <div className="col-sm-6">
+            <div>
+                <div className="row">
                     <div className="col-sm-6">
                         <Dropdown
-                            isOpen={this.state.tasksDropdownOpen}
-                            toggle={this.toggle.bind(this, 'tasksDropdownOpen')}
+                            isOpen={this.state.coursesDropdownOpen}
+                            toggle={this.toggle.bind(this, 'coursesDropdownOpen')}
                         >
-                            <DropdownToggle caret={true}>{this.state.tasksValue}</DropdownToggle>
+                            <DropdownToggle caret={true}>{this.state.coursesValue}</DropdownToggle>
                             <DropdownMenu>
-                                {this.props.tasks.map((task: any) => {
+                                {this.props.courses.map((course: any) => {
                                     return (
                                         <DropdownItem
-                                            key={task._id}
-                                            onClick={(event: any) => this.select(event, 'tasksValue')}
+                                            key={course._id}
+                                            onClick={(event: any) => this.select(event, 'coursesValue')}
                                         >
-                                            {task.name}
+                                            {course.name}
                                         </DropdownItem>
                                     );
                                 })}
                             </DropdownMenu>
                         </Dropdown>
                     </div>
+                    <div className="col-sm-6">
+                        <div className="col-sm-6">
+                            <Dropdown
+                                isOpen={this.state.tasksDropdownOpen}
+                                toggle={this.toggle.bind(this, 'tasksDropdownOpen')}
+                            >
+                                <DropdownToggle caret={true}>{this.state.tasksValue}</DropdownToggle>
+                                <DropdownMenu>
+                                    {this.props.tasks.map((task: any) => {
+                                        return (
+                                            <DropdownItem
+                                                key={task._id}
+                                                onClick={(event: any) => this.select(event, 'tasksValue')}
+                                            >
+                                                {task.name}
+                                            </DropdownItem>
+                                        );
+                                    })}
+                                </DropdownMenu>
+                            </Dropdown>
+                        </div>
+                    </div>
+                </div>
+                <div className="row dropzone">
+                    <Dropzone onDrop={this.onDrop} disabled={this.state.disabled}>
+                        {this.state.disabled ? (
+                            <p>{this.state.files[0].name}</p>
+                        ) : (
+                            <p>Try dropping some files here, or click to select files to upload.</p>
+                        )}
+                    </Dropzone>
                 </div>
             </div>
         );
